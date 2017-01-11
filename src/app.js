8637aed66dbac89e5ca42f0f5bf44d56a3b3bc11
bruceisyoung <bruce.isyoung2016@gmail.com>
2017-01-10 16:53:51 -0800

    call reddit api correctly

--- src/app.js
+++ src/app.js
@@ -1,6 +1,35 @@
 import React, { Component } from 'react';
+import snoowrap from 'snoowrap'
+
+const r = new snoowrap({
+  userAgent: 'chrome-dev-bruceisyoung',
+  clientId: '2mDqMUu3-Uf8HQ',
+  clientSecret: '5iY-hxx-fRAudzyRyyXpIoEOGas',
+  username: 'bruceisyoung',
+  password: '123456'
+});
 
 export default class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			subreddits: [],
+			submissions: []
+		}
+	}
+
+	componentWillMount() {
+		r._get({uri: 'api/v1/me/karma'}).
+			then((res) => {
+				if (res.length === 0) {
+					return r._get({uri: '/hot'});
+				} else {
+					return false;
+				}
+			}).
+			then(list => console.log('list: ', list));
+	}
+
   render() {
     return (
     	<p>I am building a reddit viewer</p>
